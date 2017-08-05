1b5f6199137f88b2279ba5b5009afbc96007a0da
N-BhanuKiran <bhanukbn@outlook.com>
2017-08-05 23:30:34 +0530

    Custom Html Customs using Extension Methods
    Created a helper class and used it in the About.cshtml

---
+++ razorBlockApp/razorBlockApp/Helpers/MyCustomStripHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace razorBlockApp.Helpers
+{
+    public static class MyCustomStripHelper
+    {
+        public static MvcHtmlString myHtmlStrip(this HtmlHelper html, string inputHtml)
+        {
+            return new MvcHtmlString(System.Text.RegularExpressions.Regex.Replace(inputHtml,"<.*?>",string.Empty));
+        }
+    }
+}
